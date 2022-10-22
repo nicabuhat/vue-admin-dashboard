1f0d62e0180c37767f05ab16dbc95de909e6e165
unknown <cabuhat.nicaela@gmail.com>
2022-10-22 23:34:10 +0800

    setup design system colors and typography

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
+</template>
+
+<style lang="scss">
+@import "@/global-styles/colors.scss";
+@import "@/global-styles/typography.scss";
+
+body {
+  background-color: $dark-blue;
+}
+
+h1 {
+  @include heading-1;
+}
+
+p {
+  @include large-text;
+}
+
+#app {
+  font-family: $sytem-font-family;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: $white;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: $white;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
